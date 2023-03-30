43eb34f7fec5f35cdd13274ae4befd47c5236e37
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-03-30 17:27:05 +0530

    backend broilerplate changed

--- booking-frontend/pages/index.js
+++ booking-frontend/pages/index.js
@@ -4,7 +4,14 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <>
-      Hello World
+      <h1>Concert</h1>
+      <p>Lorem ipsum dolor sit amet. Est corporis sint et architecto voluptatem sit voluptas cupiditate ut sunt placeat eum dignissimos saepe a explicabo accusamus id dolore sint? Aut ipsa facere aut officia atque est perferendis quia ab sapiente obcaecati.
+      Et iste repudiandae est mollitia voluptas ad mollitia minus et harum earum At voluptatem enim. Eos provident animi eum beatae omnis est temporibus molestias eum illo animi. Et quam consequatur ut Quis inventore et autem magni qui laboriosam adipisci qui placeat aperiam quo ipsam ratione eum saepe alias. Ut quisquam consectetur rem consequatur explicabo est unde similique.</p>
+      <h2>Bookings Open</h2>
+      <div className={styles.bookingContainer}>
+        <p>Book Now!</p>
+        <ConnectWallet />
+      </div>
     </>
   );
 }
